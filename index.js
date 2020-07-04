d1d8456aa32736aff9854a17bdba55a5c2559d16
Faisalpb7 <pbfaisy7@gmail.com>
2020-07-04 18:19:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#home-slider .owl-carousel").owlCarousel({
+        loop:true,
+        autoplay: true,
+        animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+        dots:true,
+        items:1
+        
+    });
+
+
+})    
